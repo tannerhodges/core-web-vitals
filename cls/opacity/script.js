629d0eff1ad062b65971e844a4fbe66f861d6c2c
Tanner Hodges <me@tannerhodges.com>
2020-10-24 19:19:58 -0400

    Content Layout Shift + Opacity

---
+++ cls/opacity/script.js
@@ -0,0 +1,2 @@
+setTimeout(() => test.style.height = '100px', 1000);
+setTimeout(() => document.body.style.opacity = '1', 1500);
---
+++ cls/visibility/script.js
@@ -0,0 +1,5 @@
+setTimeout(() => test.style.height = '100px', 1000);
+setTimeout(() => {
+	document.body.style.opacity = '1';
+	document.body.style.visibility = 'visible';
+}, 1500);
