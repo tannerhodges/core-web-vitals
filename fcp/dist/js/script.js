6497bc8d959447f95cd037875463d313aa513a33
Tanner Hodges <me@tannerhodges.com>
2020-10-26 21:31:54 -0400

    Fix iframe URL on demo environment

--- fcp/dist/js/script.js
+++ fcp/dist/js/script.js
@@ -1,7 +1,7 @@
 // Add an iframe.
 var e = document.createElement('iframe');
 e.style.display = 'none';
-e.src = '/fcp/dist/iframe.html';
+e.src = './dist/iframe.html';
 document.body.appendChild(e);
 
 // Show the page.
